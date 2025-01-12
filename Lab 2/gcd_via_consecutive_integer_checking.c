ebb2633c90e7b5446138526307068d915557e1b7
Aditya Mathpal <adimathpal@gmail.com>
2025-01-12 15:39:17 +0530

    updated ex1

--- Lab 2/gcd_via_consecutive_integer_checking.c
+++ Lab 2/gcd_via_consecutive_integer_checking.c
@@ -25,4 +25,7 @@ int main() {
 output:
 Enter two numbers to calculate GCD (do not enter 0 or lesser): 12 16
 GCD is 4
+
+time efficiency:
+Since it's a brute-force approach, it's quite inefficient as compared to Euclid's algorithm, it has a time complexity of O(min(N,M)) where N and M are the inputs.
 */
