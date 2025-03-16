f69bdbec3b3e2e585b4dc300ad28fd4b99374518
Aditya Mathpal <adimathpal@gmail.com>
2025-03-16 15:38:26 +0530

    added lab 9 ex1

--- Lab 09/horspool.c
+++ Lab 09/horspool.c
@@ -7,11 +7,11 @@ void shiftTable(char* pattern, int* table) {
     for(i = 0; i < m-1; i++) table[pattern[i]] = m - 1 - i;
 }
 
-int horspool(char* text, char* pattern, int* table) {
+int horspool(char* text, char* pattern, int* table, int* comp) {
     shiftTable(pattern, table);
     int m = strlen(pattern), n = strlen(text), k, i;
     for(int i = m - 1; i < n; i += table[(unsigned char)text[i]]) {
-        for(k = 0; k < m && pattern[m - 1 - k] == text[i - k]; k++);
+        for(k = 0; k < m && pattern[m - 1 - k] == text[i - k]; k++) (*comp)++;
         if(k == m) return i - m + 1;
     }
     return -1;
@@ -19,22 +19,30 @@ int horspool(char* text, char* pattern, int* table) {
 
 int main() {
     char text[1000], pattern[100];
-    int table[256];
+    int table[256], comp = 0;
     printf("Enter the string to be searched in: ");
     fgets(text, 1000, stdin);
     printf("Enter string to be searched for: ");
     fgets(pattern, 100, stdin);
     text[strcspn(text, "\n")] = pattern[strcspn(pattern, "\n")] = 0;
 
-    int index = horspool(text, pattern, table);
-    if(index != -1) printf("Pattern found at index %d.\n", index);
-    else printf("Pattern not found in string.\n");
+    int index = horspool(text, pattern, table, &comp);
+    if(index != -1) printf("Pattern found at index %d\n", index);
+    else printf("Pattern not found in string\n");
+    printf("Number of comparisons: %d\n", comp);
     return 0;
 }
 
 /*
-output:
-Enter the string to be searched in: the quick brown fox jumps over the lazy dog
-Enter string to be searched for: ck br 
-Pattern found at index 7.
+output (successful search):
+Enter the string to be searched in: test string
+Enter string to be searched for: t st
+Pattern found at index 3
+Number of comparisons: 6
+
+output (unsuccessful search):
+Enter the string to be searched in: test string
+Enter string to be searched for: abcd
+Pattern not found in string
+Number of comparisons: 0
 */
